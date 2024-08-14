2a1ababd7cbead92cdeb46e70c83d0bcfce078d8
bwilbert67 <bwilbert67@gmail.com>
2024-08-14 19:53:07 -0400

    chagned port for local

--- src/services/Config.js
+++ src/services/Config.js
@@ -4,7 +4,7 @@ const getBaseURL = () => {
     
     if (isLocalhost) {
         // If the UI is running on localhost, use the local backend URL
-        return 'http://127.0.0.1:5000';  // Replace with your actual local BFF URL if different
+        return 'http://127.0.0.1:8080';  // Replace with your actual local BFF URL if different
     } else {
         // If the UI is not on localhost, use the production API Gateway URL
         return 'http://budget-env.eba-cnytm39p.us-east-2.elasticbeanstalk.com/';
