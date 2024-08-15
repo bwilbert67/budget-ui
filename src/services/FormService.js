7736f2a6331279b8155a9486bfe1a299f568e869
bwilbert67 <bwilbert67@gmail.com>
2024-08-14 21:28:19 -0400

    getting from all endpoints

--- src/services/FormService.js
+++ src/services/FormService.js
@@ -39,4 +39,82 @@ export async function getCategories() {
         return [];
     }
 }
+export async function getPurchases() {
+    try {
+        const response = await fetch(`${baseURL}/form/purchases`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        
+        const purchases = await response.json();
+        return purchases;
+    } catch (error) {
+        console.error('Error retrieving purchases:', error);
+        return [];
+    }
+}
+export async function getBudgetLeft() {
+    try {
+        const response = await fetch(`${baseURL}/form/budget-left`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        
+        const budgetData = await response.json();
+        return budgetData;
+    } catch (error) {
+        console.error('Error retrieving budget left:', error);
+        return null;
+    }
+}
+export async function updateMonthlyData(data) {
+    try {
+        const response = await fetch(`${baseURL}/form/update-monthly-data`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        
+        const result = await response.json();
+        console.log('Update Monthly Data Response:', result);
+    } catch (error) {
+        console.error('Error updating monthly data:', error);
+    }
+}
+export async function getMonthlyData() {
+    try {
+        const response = await fetch(`${baseURL}/form/get-monthly-data`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        
+        const monthlyData = await response.json();
+        return monthlyData;
+    } catch (error) {
+        console.error('Error retrieving monthly data:', error);
+        return null;
+    }
+}
+
+
 
