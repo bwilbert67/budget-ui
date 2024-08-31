0b46500c3fcb0ed19b8f962d7036e08f01c6fea2
bwilbert67 <bwilbert67@gmail.com>
2024-08-31 18:46:01 -0400

    added logic for logging in

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './views/HomePage';
 import './index.css';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { getToken } from './services/AuthService';
 
 const theme = createTheme({
     palette: {
         primary: {
-            main: '#4CAF50', 
+            main: '#4CAF50',
         },
         secondary: {
-            main: '#FFC107', 
-        }
+            main: '#FFC107',
+        },
     },
     typography: {
-        fontFamily: 'Roboto, sans-serif',
+        fontFamily: "Times New Roman, Garamond",
     },
 });
 
 const App = () => {
+    // Placeholder for the token, remove this line when implementing real login
+    const placeholderToken = "fake-token"; // This line is temporary
+
+    // Use the placeholder token or the actual token from localStorage
+    const token = placeholderToken || localStorage.getItem('jwt');
+
+    const [isAuthenticated, setIsAuthenticated] = useState(!!token);
+
+    useEffect(() => {
+        // For actual login, remove the use of the placeholderToken
+        if (token) {
+            setIsAuthenticated(true);
+        } else {
+            setIsAuthenticated(false);
+        }
+    }, [token]);
+
     return (
-        // Wrap the app with ThemeProvider and pass the theme
         <ThemeProvider theme={theme}>
             <Router>
                 <Routes>
-                    <Route path="/" element={<HomePage />} />
+                    {isAuthenticated ? (
+                        <Route path="/" element={<HomePage />} />
+                    ) : (
+                        <Route path="/" element={<h1>Please put the login page here </h1>} />
+                    )}
                 </Routes>
             </Router>
         </ThemeProvider>
